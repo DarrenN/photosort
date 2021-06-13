002d36539dabb83d1798487febfb0dcda5981277
DarrenN <info@v25media.com>
2021-06-13 16:03:33 -0400

    Adds JSON logger and dispatch table

--- photosort.scm
+++ photosort.scm
@@ -1,18 +1,29 @@
 (import (chicken condition)
         (chicken file)
         (chicken format)
+        (chicken io)
         (chicken pathname)
         (chicken platform)
         (chicken process-context)
+        (chicken string)
+        (chicken syntax)
+        defstruct
         exif
         image-dimensions
+        json
         list-utils
+        matchable
         simple-sha1
         sql-de-lite
         srfi-1
         srfi-13
         srfi-14
+        srfi-19-core
+        srfi-19-io
         srfi-69)
+
+(import-for-syntax (chicken string)
+                   srfi-13)
 
 ;; 1. pull filenames from Camera Uploads dir
 ;; 2. use simple-sha1, exif and image-dimensions to get the sha1sum,
@@ -24,19 +35,66 @@
 ;; 7. Add row in sqlite DB with: sha, filename, new path, datetime,
 ;;    width, height, type
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Logging
 
-(define (%log p fmt r)
-  (let ((out (if (port? p) p (current-error-port))))
-    (apply fprintf p fmt r)
-    (apply fprintf p "\n" '())))
+(define TRACE "TRACE")
+(define DEBUG "DEBUG")
+(define INFO  "INFO")
+(define WARN  "WARN")
+(define ERROR "ERROR")
+(define FATAL "FATAL")
 
-(define (log-err fmt #!rest r #!key (p (current-error-port)))
-  (%log p (string-append "ERR: " fmt) r))
+(define *default-log-level* (make-parameter INFO))
+(define *default-log-stream* (make-parameter (current-output-port)))
 
-(define (log-debug fmt #!rest r #!key (p (current-output-port)))
-  (%log p (string-append "DEBUG: " fmt) r))
+(define (%json-log p h)
+  (let ((out (if (port? p) p (*default-log-stream*)))
+        (o (if (hash-table? h) h (make-hash-table eq? symbol-hash))))
+    (hash-table-set! o 'timestamp (timestamp))
+    (hash-table-update!/default o 'level identity (*default-log-level*))
+    (json-write o out)
+    (write-line "" out)))
+
+(define (->symbol x)
+  (cond [(symbol? x) x]
+        [(string? x) (string->symbol x)]
+        [(number? x) (string->symbol (number->string x))]
+        [else (gensym "invalid-field")]))
+
+;; TODO: add checks for proper length
+(define (list->hash l)
+  (define (inner ps l)
+    (if (null? l)
+        (alist->hash-table ps #:test eq? #:hash symbol-hash)
+        (inner
+         (append ps (list (cons (->symbol (car l)) (cadr l)))) (drop l 2))))
+  (inner '() l))
+
+(define-syntax define-logger
+  (er-macro-transformer
+   (lambda (exp rename compare)
+     (let* ((level-id (cadr exp))
+            (level-label (string-upcase (symbol->string level-id)))
+            (proc-id (string->symbol
+                      (conc "log-" (symbol->string level-id))))
+            (%define (rename 'define))
+            (%let (rename 'let))
+            (%hash-table-set! (rename 'hash-table-set!))
+            (%json-log (rename '%json-log))
+            (%list->hash (rename 'list->hash)))
+       `(,%define (,proc-id . kvs)
+                  (,%let ((h (,%list->hash kvs)))
+                         (,%hash-table-set! h 'level ,level-label)
+                         (,%json-log '() h)))))))
+
+(define-logger trace)
+(define-logger info)
+(define-logger debug)
+(define-logger warn)
+(define-logger error)
+(define-logger fatal)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cache filepaths
@@ -186,6 +244,12 @@ new_path = ? WHERE hash = ?;")
          (date (string-tokenize (car tokens) char-set:digit)))
     (string-append (string-join date "-") " " (cadr tokens) ".000")))
 
+;; ISO 8601 of the current UTC date/time w nanoseconds
+(define (timestamp)
+  (date->string
+   (current-date (utc-timezone-locale)) "~Y-~m-~dT~H:~M:~S.~N~z"))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Image file handling
 
@@ -242,13 +306,6 @@ new_path = ? WHERE hash = ?;")
         (log-err "no exif for ~A" path)))
     #t)
 
-(define ((process-dir db in out) path prev)
-  (if (file-exists? path)
-      (process-image db path out)
-      (begin
-        (log-err "file doesn't exist: ~A" path)
-        #f)))
-
 ;; We're only interested in certain types of files
 ;; primarily JPGs Maybe mp4 / mov?
 (define (is-jpeg? path)
@@ -258,6 +315,69 @@ new_path = ? WHERE hash = ?;")
                  (string= ext "jpeg")))
         #t
         #f)))
+
+(define (check-file-path p)
+  (let ((f (photo-filename p)))
+    (if (and f (file-exists? f))
+        (dispatch-extract-tags p)
+        (dispatch-error (format "~A is not a valid file" f)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Handlers
+
+(defstruct photo db filename input-dir output-dir)
+
+(define ((dispatch-photo db in out) path prev)
+  (dispatch-begin
+   (make-photo db: db
+               input-dir: in
+               output-dir: out
+               filename: path)))
+
+(define (handle-error xs)
+  (log-error 'msg (format "~A" xs)))
+
+(define (stub p)
+  (log-debug 'msg (format "Stub called with ~A" (photo->alist p))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Dispatching
+
+(define verbose? #f)
+
+(define-syntax wrap-debug
+  (syntax-rules ()
+    ((_ fn var)
+     (begin
+       (when verbose?
+         (log-debug 'proc 'fn 'msg (format "args: ~A" var)))
+       (fn var)))))
+
+(define-syntax define-dispatch
+  (er-macro-transformer
+   (lambda (exp rename compare)
+     (let* ((dispatch-id (cadr exp))
+            (new-id (string->symbol
+                     (conc "dispatch-" (symbol->string dispatch-id))))
+            (%define (rename 'define))
+            (%dispatch (rename 'dispatch)))
+       `(,%define (,new-id p) (,%dispatch (list ',dispatch-id p)))))))
+
+(define (dispatch photo)
+  (when verbose?
+    (log-debug 'msg (format "Dispatch (dispatch ~A)" photo)))
+  (handle-exceptions
+      exn
+      (dispatch-error ((condition-property-accessor 'exn 'message) exn))
+    (match photo
+      [('begin (? photo? p)) (wrap-debug check-file-path p)]
+      [('success (? photo? p)) (wrap-debug stub p)]
+      [('error . xs) (wrap-debug handle-error xs)]
+      [_ (log-error 'msg (format "No match for ~A" photo))])))
+
+(define-dispatch begin)
+(define-dispatch extract-tags)
+(define-dispatch error)
 
 ;; Accepts two command line args:
 ;;
@@ -278,8 +398,8 @@ new_path = ? WHERE hash = ?;")
         (let ((db (get-db-handle)))
           (find-files input-dir
                       #:test is-jpeg?
-                      #:action (process-dir db input-dir output-dir))
+                      #:action (dispatch-photo db input-dir output-dir))
           (close-database db)))
-      (log-err "input: ~A output: ~A" input-dir output-dir)))
+      (log-error 'msg (format "input: ~A output: ~A" input-dir output-dir))))
 
 
